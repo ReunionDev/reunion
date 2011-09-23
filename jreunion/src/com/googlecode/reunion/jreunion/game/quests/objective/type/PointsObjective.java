0902c1f22a6f60d30b10a4eec02a3f599a4018fe
bigsam <samuel.basto@gmail.com>
2011-09-23 15:54:50 +0000

    git-svn-id: http://reunion.googlecode.com/svn/trunk@263 d5b2e400-7aba-11de-9499-4f8beaa274e3

--- jreunion/src/com/googlecode/reunion/jreunion/game/quests/objective/type/PointsObjective.java
+++
@@ -1,8 +0,0 @@
-package com.googlecode.reunion.jreunion.game.quests.objective.type;
-
-public class PointsObjective extends ObjectiveType{
-	
-	public PointsObjective(int type) {
-		super(type);
-	}
-}
