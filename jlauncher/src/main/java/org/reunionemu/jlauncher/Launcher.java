12d9d9759f4d88b39522f798bc660c8ba48d1877
Aidamina <aidamina@gmail.com>
2011-11-24 14:15:16 +0100

    merge launcher

---
+++ jlauncher/src/main/java/org/reunionemu/jlauncher/Launcher.java
@@ -0,0 +1,67 @@
+package org.reunionemu.jlauncher;
+
+import java.io.File;
+import java.net.InetAddress;
+import java.util.Iterator;
+
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.spi.LoggingEvent;
+import org.reunionemu.jcommon.ParsedItem;
+import org.reunionemu.jcommon.Parser;
+import org.reunionemu.jcommon.ServerList;
+import org.reunionemu.jcommon.ServerList.ServerListItem;
+
+public class Launcher {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Logger logger = Logger.getRootLogger();
+		logger.addAppender(new ConsoleAppender(new PatternLayout("%-5p [%t]: %m\r\n"){			
+			@Override
+			public String format(LoggingEvent event) {
+
+				String result = super.format(event);
+				if(result.endsWith("\n\r\n")){
+					
+					result = result.substring(0, result.length()-2);
+				}
+				return result;
+			}
+			
+		}, ConsoleAppender.SYSTEM_OUT));
+		Parser launcher = new Parser();
+		Parser servers = new Parser();
+		ServerList serverList = new ServerList();
+		try {
+			launcher.Parse("Launcher.dta");		
+			servers.Parse("Servers.dta");
+			Iterator<ParsedItem> iter = servers.getItemListIterator();
+			while(iter.hasNext()) {
+				ParsedItem server = iter.next();
+				serverList.getItems().add(new ServerListItem(server.getName(), InetAddress.getByName(server.getMemberValue("Address")), Integer.parseInt(server.getMemberValue("Port"))));				
+			}
+			serverList.Save("SvrList.dta");
+			
+			String dir = ".";
+			dir = "E:\\Games\\BiosFear\\";
+			
+			String version = launcher.getItem("Launcher").getMemberValue("Version");
+			ProcessBuilder builder = new ProcessBuilder(new String[] { "cmd.exe", "/C", "Game.exe "+version });
+			builder.directory(new File(dir));
+			
+			Process p = builder.start();
+			//Runtime.getRuntime().exec("E:\\Games\\BiosFear\\game.exe 100");
+			//System.out.println(p.waitFor());
+			
+			
+		} catch (Exception e) {
+			Logger.getLogger(Launcher.class).warn("Exception",e);
+		}
+
+	}
+
+}
