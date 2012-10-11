e2cc4f5d6dc3e1f7bb118cfc5b2a3caa6f29f07a
Tjeerd Jan van der Molen <aidamina@gmail.com>
2012-10-11 16:16:47 +0200

    * added some package information

---
+++ jreunion/src/main/java/org/reunionemu/jreunion/dao/jpa/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Spring data jpa dao classes need to be in this package to be picked up by the package scanner
+ */
+/**
+ * @author Aidamina
+ *
+ */
+package org.reunionemu.jreunion.dao.jpa;
---
+++ jreunion/src/main/java/org/reunionemu/jreunion/model/jpa/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Hibernate annotated classes need to be in this package to be picked up by the package scanner
+ */
+/**
+ * @author Aidamina
+ *
+ */
+package org.reunionemu.jreunion.model.jpa;
