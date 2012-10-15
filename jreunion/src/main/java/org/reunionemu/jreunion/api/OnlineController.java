37bebfa67a78c960bb735509ed595106b6aab177
Aidamina <aidamina@gmail.com>
2012-10-15 22:07:10 +0200

    * PlayerController /api/players/{mapId} /api/players

--- jreunion/src/main/java/org/reunionemu/jreunion/api/OnlineController.java
+++ jreunion/src/main/java/org/reunionemu/jreunion/api/OnlineController.java
@@ -2,7 +2,10 @@ package org.reunionemu.jreunion.api;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
+import org.reunionemu.jreunion.game.Player;
 import org.reunionemu.jreunion.server.LocalMap;
 import org.reunionemu.jreunion.server.Map;
 import org.reunionemu.jreunion.server.Server;
@@ -14,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/online")
+@RequestMapping("/players")
 public class OnlineController {
 	
 	@Autowired
@@ -22,31 +25,62 @@ public class OnlineController {
 	
 	//@Cacheable(value="default")
 	@RequestMapping(value="{mapId}", method = RequestMethod.GET)
-	public @ResponseBody int getCount(@PathVariable int mapId) {
+	public @ResponseBody List<Object> getCount(@PathVariable int mapId) {
 		
 		Map map = server.getWorld().getMap(mapId);
 		if(map!=null && map instanceof LocalMap){
-			return ((LocalMap) map).getPlayerList().size();
+			
+			List<Object> playersReponse = new LinkedList<Object>();
+			
+			List<Player> players = ((LocalMap) map).getPlayerList();
+			
+			for(final Player player: players){
+				
+				playersReponse.add(buildResponse(player));				
+				
+			}
+			
+			return playersReponse;
 		}
 		throw new RuntimeException("");
 	}
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public @ResponseBody int getCountTotal() {
+	public @ResponseBody List<Object> getCountTotal() {
  
 		Collection<Map> maps = server.getWorld().getMaps();
-		Iterator<Map> iter = maps.iterator();
-		int total = 0;
-		
+		Iterator<Map> iter = maps.iterator();		
+		List<Object> playersReponse = new LinkedList<Object>();
 		while(iter.hasNext()){
 			Map map = iter.next();
 
 			if( map instanceof LocalMap){
-				total += ((LocalMap) map).getPlayerList().size();
+				List<Player> players = ((LocalMap) map).getPlayerList();
+
+				for(final Player player: players){
+					
+					playersReponse.add(buildResponse(player));				
+					
+				}
 			}
 			
 		}
-		return total;
+		return playersReponse;
+	}
+	
+	@SuppressWarnings("unused")
+	public Object buildResponse(final Player player){
+		return new Object(){
+			
+			public String name = player.getName();
+			public int level = player.getLevel();
+			public Object position = new Object(){
+				public int x = player.getPosition().getX();
+				public int y = player.getPosition().getY();
+				public int z = player.getPosition().getZ();
+				public int map = player.getPosition().getMap().getId();
+			};
+		};
 	}
 
 }
