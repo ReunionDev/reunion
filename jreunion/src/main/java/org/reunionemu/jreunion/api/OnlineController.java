1966b666052ef6a7b077f23222dea7864047c324
Tjeerd Jan van der Molen <aidamina@gmail.com>
2012-10-16 09:14:35 +0200

    * swapped PlayerController and OnlineController

--- jreunion/src/main/java/org/reunionemu/jreunion/api/OnlineController.java
+++ jreunion/src/main/java/org/reunionemu/jreunion/api/OnlineController.java
@@ -2,10 +2,7 @@ package org.reunionemu.jreunion.api;
 
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 
-import org.reunionemu.jreunion.game.Player;
 import org.reunionemu.jreunion.server.LocalMap;
 import org.reunionemu.jreunion.server.Map;
 import org.reunionemu.jreunion.server.Server;
@@ -17,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/players")
+@RequestMapping("/online")
 public class OnlineController {
 	
 	@Autowired
@@ -25,62 +22,31 @@ public class OnlineController {
 	
 	//@Cacheable(value="default")
 	@RequestMapping(value="{mapId}", method = RequestMethod.GET)
-	public @ResponseBody List<Object> getCount(@PathVariable int mapId) {
+	public @ResponseBody int getCount(@PathVariable int mapId) {
 		
 		Map map = server.getWorld().getMap(mapId);
 		if(map!=null && map instanceof LocalMap){
-			
-			List<Object> playersReponse = new LinkedList<Object>();
-			
-			List<Player> players = ((LocalMap) map).getPlayerList();
-			
-			for(final Player player: players){
-				
-				playersReponse.add(buildResponse(player));				
-				
-			}
-			
-			return playersReponse;
+			return ((LocalMap) map).getPlayerList().size();
 		}
 		throw new RuntimeException("");
 	}
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public @ResponseBody List<Object> getCountTotal() {
+	public @ResponseBody int getCountTotal() {
  
 		Collection<Map> maps = server.getWorld().getMaps();
-		Iterator<Map> iter = maps.iterator();		
-		List<Object> playersReponse = new LinkedList<Object>();
+		Iterator<Map> iter = maps.iterator();
+		int total = 0;
+		
 		while(iter.hasNext()){
 			Map map = iter.next();
 
 			if( map instanceof LocalMap){
-				List<Player> players = ((LocalMap) map).getPlayerList();
-
-				for(final Player player: players){
-					
-					playersReponse.add(buildResponse(player));				
-					
-				}
+				total += ((LocalMap) map).getPlayerList().size();
 			}
 			
 		}
-		return playersReponse;
-	}
-	
-	@SuppressWarnings("unused")
-	public Object buildResponse(final Player player){
-		return new Object(){
-			
-			public String name = player.getName();
-			public int level = player.getLevel();
-			public Object position = new Object(){
-				public int x = player.getPosition().getX();
-				public int y = player.getPosition().getY();
-				public int z = player.getPosition().getZ();
-				public int map = player.getPosition().getMap().getId();
-			};
-		};
+		return total;
 	}
 
 }
--- jreunion/src/test/java/org/reunionemu/jreunion/server/database/model/QuestStateDaoTest.java
+++ jreunion/src/test/java/org/reunionemu/jreunion/server/database/model/QuestStateDaoTest.java
@@ -10,9 +10,9 @@ import org.reunionemu.jreunion.dao.QuestDao;
 import org.reunionemu.jreunion.dao.QuestStateBaseDao;
 import org.reunionemu.jreunion.game.quests.QuestState;
 import org.reunionemu.jreunion.model.Quest;
+import org.reunionemu.jreunion.model.jpa.CounterObjectiveStateImpl;
 import org.reunionemu.jreunion.model.jpa.QuestStateImpl;
-import org.reunionemu.jreunion.model.jpa.test.CounterObjectiveStateEx;
-import org.reunionemu.jreunion.model.jpa.test.ObjectiveState;
+import org.reunionemu.jreunion.model.quests.CounterObjectiveState;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -35,11 +35,17 @@ public class QuestStateDaoTest {
 		assumeNotNull(questDao);
 		Quest quest = questDao.findById(1);
 		assumeNotNull(quest);
-		
+		System.out.println("create");
 		QuestState state = questStateDao.create(quest);
 		//QuestStateImpl state = new QuestStateImpl(quest);
 		
 		assertNotNull(state);
+		System.out.println("save");
+		questStateDao.save(state);
+		System.out.println("save");
+		questStateDao.save(state);
+		System.out.println("save");
+		((CounterObjectiveState)state.getObjectives().get(0)).decrease();
 		questStateDao.save(state);
 		
 		QuestState qs = questStateDao.findOne(((QuestStateImpl)state).getId());
