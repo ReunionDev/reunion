267a4e6ea5ddb7834708a1ce51b7e373faf54990
Tjeerd Jan van der Molen <aidamina@gmail.com>
2012-10-16 16:49:22 +0200

    test

--- jreunion/src/main/java/org/reunionemu/jreunion/model/jpa/ItemImpl.java
+++ jreunion/src/main/java/org/reunionemu/jreunion/model/jpa/ItemImpl.java
@@ -10,62 +10,60 @@ import javax.persistence.UniqueConstraint;
 import org.reunionemu.jreunion.game.Item;
 import org.reunionemu.jreunion.game.ItemType;
 
-
-
 @Entity
 @Table(name="items",
 uniqueConstraints={
 		@UniqueConstraint(columnNames = { "id" })
 })
 public class ItemImpl<T extends ItemType> extends Item<T> {
-	public ItemImpl(T itemType) {
+	Long id;
+
+	
+    Integer typeId;
+	
+    Integer durability;
+    
+    Long gemNumber;
+    
+    public ItemImpl(T itemType) {
 		super(itemType);
 	}
 
+    @Column(name="durability")
+	public Integer getDurability() {
+		return durability;
+	}
+
+	@Column(name="gemnumber")
+	public long getGemNumber() {
+		return gemNumber;
+	}
 	
-    Long id;
-	
-    @Id @GeneratedValue
+	@Id @GeneratedValue
 	public Long getId(){
 		return id;
 	}
-    
-    public void setId(Long id){
-    	this.id = id;
-    }
-    
-    Integer typeId;
-
-    @Column(name="type_id")
+	
+	@Column(name="type_id")
 	public Integer getTypeId() {
 		return typeId;
-	}
-
-	public void setTypeId(Integer typeId) {
-		this.typeId = typeId;
-	}
-	
-	Integer durability;
-	
-	@Column(name="durability")
-	public Integer getDurability() {
-		return durability;
 	}
 
 	public void setDurability(Integer durability) {
 		this.durability = durability;
 	}
 	
-	@Column(name="gemnumber")
-	public long getGemNumber() {
-		return gemNumber;
-	}
-
 	public void setGemNumber(Long gemNumber) {
 		this.gemNumber = gemNumber;
 	}
 
-	Long gemNumber;
+	public void setId(Long id){
+    	this.id = id;
+    }
+
+	public void setTypeId(Integer typeId) {
+		this.typeId = typeId;
+	}
 
 	
 }
