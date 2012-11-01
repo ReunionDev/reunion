28bbd43d499ef97b6f093b670616f04b3bbea649
Tjeerd Jan van der Molen <aidamina@gmail.com>
2012-11-01 16:40:37 +0100

    * scheduled test

--- jreunion/src/test/java/SchedulerTest.java
+++ jreunion/src/test/java/SchedulerTest.java
@@ -15,8 +15,9 @@ public class SchedulerTest {
 	/**
 	 * @param args
 	 * @throws InterruptedException 
+	 * @throws ExecutionException 
 	 */
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 				
 		ManualScheduledExecutor executor = new ManualScheduledExecutor();
 		
@@ -28,15 +29,27 @@ public class SchedulerTest {
 				
 			}
 		}, 0, 1, TimeUnit.SECONDS);
-		
+	    
+	    Future<Integer> solution =  executor.submit(new Callable<Integer>() {
+	    	
+	    	@Override
+	    	public Integer call() throws Exception {
+	    		return 1337;
+	    	}
+		});
+	   
+	    
+	    System.out.println(solution.isDone());
+	    
+	    
 	    // This will make the executor accept no new threads
 	    // and finish all existing threads in the queue
 	    //executor.shutdown();
 	    Runnable task = null;
 	    while(true){
+	    	System.out.println(solution.isDone());
 		    while((task = executor.getQueue().poll())!=null){
 		    	task.run();
-		    	
 		    	
 		    }
 		    Thread.sleep(100);
