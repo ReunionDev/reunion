0e2c18af017901acb12ddf3c469f3e8568ccce72
Tjeerd Jan van der Molen <aidamina@gmail.com>
2012-11-02 16:29:05 +0100

    * protocol factory

--- jreunion/src/test/java/netty/NettyServer.java
+++ jreunion/src/test/java/netty/NettyServer.java
@@ -4,61 +4,85 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.*;
-import io.netty.handler.codec.http.*;
 
-public class NettyServer {
+public class NettyServer implements ProtocolFactory, Protocol {
 
 	public NettyServer() {
-		// TODO Auto-generated constructor stub
+   
 	}
 
+	
+	public void start() throws Exception{
+		
+		 ServerBootstrap b = new ServerBootstrap();
+			
+		    try{
+			    b.group(new NioEventLoopGroup(), new NioEventLoopGroup())
+			  //  .channel(new NioServerSocketChannel())
+			    .channel(NioServerSocketChannel.class)
+		        .option(ChannelOption.SO_BACKLOG, 100)
+			    .localAddress(4005)
+			    .childHandler(new ChannelInitializer<SocketChannel>() {
+		             @Override
+		             public void initChannel(SocketChannel ch) throws Exception {
+		            	 ch.pipeline().addLast("codec", new ProtocolCodec(NettyServer.this));
+		            	 ch.pipeline().addLast("handler", new ChannelInboundMessageHandlerAdapter<String>(){
+
+							@Override
+							public void messageReceived(
+									ChannelHandlerContext ctx, String msg)
+									throws Exception {
+								
+								System.out.println(msg);
+						        ctx.write("test");
+						        ctx.flush();
+						        
+						        //.addListener(ChannelFutureListener.CLOSE);
+						        
+								//System.out.println(msg);
+								//ctx.channel().write("test".getBytes());
+							}		            		 
+		            	 });
+		            	 
+		             }
+		         });
+				
+			    
+			    ChannelFuture f = b.bind().sync();
+		
+		        // Wait until the server socket is closed.
+		        f.channel().closeFuture().sync();
+		        
+		    } finally {
+		        // Shut down all event loops to terminate all threads.
+		        b.shutdown();
+		    }
+		
+		
+	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
-	    ServerBootstrap b = new ServerBootstrap();
-		
-	    try{
-		    b.group(new NioEventLoopGroup(), new NioEventLoopGroup())
-		  //  .channel(new NioServerSocketChannel())
-		    .channel(NioServerSocketChannel.class)
-	        .option(ChannelOption.SO_BACKLOG, 100)
-		    .localAddress(4005)
-		    .childHandler(new ChannelInitializer<SocketChannel>() {
-	             @Override
-	             public void initChannel(SocketChannel ch) throws Exception {
-	            	 ch.pipeline().addLast("decoder", new HttpRequestDecoder());
-	            	 ch.pipeline().addLast("encoder", new HttpResponseEncoder());
-	            	 ch.pipeline().addLast("handler", new ChannelInboundMessageHandlerAdapter<HttpRequest>(){
+		new NettyServer().start();
 
-						@Override
-						public void messageReceived(
-								ChannelHandlerContext ctx, HttpRequest msg)
-								throws Exception {
-							
-					        HttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.CREATED);
-					        
-					        ctx.write(response).addListener(ChannelFutureListener.CLOSE);
-					        
-							//System.out.println(msg);
-							//ctx.channel().write("test".getBytes());
-						}		            		 
-	            	 });
-	            	 
-	             }
-	         });
-			
-		    
-		    ChannelFuture f = b.bind().sync();
-	
-	        // Wait until the server socket is closed.
-	        f.channel().closeFuture().sync();
-	        
-	    } finally {
-	        // Shut down all event loops to terminate all threads.
-	        b.shutdown();
-	    }
+	}
 
+	@Override
+	public Protocol getProtocol(Channel channel) {
+		return this;
+	}
+
+
+	@Override
+	public byte encode(char c) {
+		return (byte)c;
+	}
+
+
+	@Override
+	public char decode(byte b) {
+		return (char)b;
 	}
 
 }
