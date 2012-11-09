2aec256913aef673fab31d4b4796bd1b45dc8192
Tjeerd Jan van der Molen <aidamina@gmail.com>
2012-11-09 10:15:38 +0100

    * out packet

---
+++ jreunion/src/test/java/netty/parsers/test/OutParserTest.java
@@ -0,0 +1,38 @@
+package netty.parsers.test;
+
+import static org.junit.Assert.*;
+
+import java.util.regex.*;
+
+import netty.Packet;
+import netty.packets.OutPacket;
+import netty.packets.OutPacket.EntityType;
+import netty.parsers.OutParser;
+
+import org.junit.Test;
+
+public class OutParserTest {
+
+	@Test
+	public void test() {
+		OutParser parser = new OutParser();
+		Pattern pattern = parser.getPattern();
+		
+		String msg = "out item 10";
+		Matcher matcher = pattern.matcher(msg);
+		assertTrue(matcher.matches());	
+		
+		Packet packet = parser.parse(matcher, msg);
+		assertNotNull(packet);
+		assertTrue(packet instanceof OutPacket);
+		
+		assertEquals(msg, packet.toString());
+		OutPacket outPacket = (OutPacket)packet;
+		assertEquals(10, outPacket.getId());
+		assertEquals(EntityType.ITEM, outPacket.getEntityType());
+		
+	}
+	
+
+
+}
