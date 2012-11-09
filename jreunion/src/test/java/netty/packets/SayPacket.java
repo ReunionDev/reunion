1adf4f509b1b993462325bdad3bec89ade88b4f9
Tjeerd Jan van der Molen <aidamina@gmail.com>
2012-11-09 14:02:28 +0100

    * say packet * party disband

---
+++ jreunion/src/test/java/netty/packets/SayPacket.java
@@ -0,0 +1,43 @@
+package netty.packets;
+
+import netty.Packet;
+
+public class SayPacket implements Packet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	int id;
+	
+	String message;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public SayPacket(){
+		
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("say ");
+		builder.append(getId());
+		builder.append(' ');
+		builder.append(getMessage());		
+		return builder.toString();
+	}
+
+}
---
+++ jreunion/src/test/java/netty/parsers/PartyDisbandParser.java
@@ -0,0 +1,28 @@
+package netty.parsers;
+
+import java.util.regex.*;
+
+import netty.Packet;
+import netty.packets.PartyDisbandPacket;
+import netty.parsers.PacketParser.Server;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+@Service
+@Scope("prototype")
+@Server
+public class PartyDisbandParser implements PacketParser {
+
+	static final Pattern regex = Pattern.compile("^party disband$"); 
+	
+	@Override
+	public Pattern getPattern() {
+		return regex;
+	}
+
+	@Override
+	public Packet parse(Matcher match, String input) {
+		return new PartyDisbandPacket();
+	}
+}
