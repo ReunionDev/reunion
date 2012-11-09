23656abf48f4f1b98bc5592b86c4e1f6aebdce6b
Tjeerd Jan van der Molen <aidamina@gmail.com>
2012-11-09 16:11:44 +0100

    * levelup packet

---
+++ jreunion/src/test/java/netty/packets/LevelUpPacket.java
@@ -0,0 +1,39 @@
+package netty.packets;
+
+import netty.Packet;
+
+import org.reunionemu.jreunion.game.Player.Status;
+
+public class LevelUpPacket implements Packet {
+	
+	private static final long serialVersionUID = 1L;
+
+	public LevelUpPacket(){
+		
+	}
+	
+	public LevelUpPacket(Status statusType){
+		
+		this.statusType = statusType;
+	}
+
+	Status statusType;
+	
+	
+	public Status getStatusType() {
+		return statusType;
+	}
+
+	public void setStatusType(Status statusType) {
+		this.statusType = statusType;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("levelup ");
+		builder.append(getStatusType().value() - 10);
+		return builder.toString();
+	}
+	
+}
