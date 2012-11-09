3c48190285c4c59ab2d8c4c85892d876f33fed86
Tjeerd Jan van der Molen <aidamina@gmail.com>
2012-11-09 15:06:29 +0100

    * start_game packet

---
+++ jreunion/src/test/java/netty/packets/StartGamePacket.java
@@ -0,0 +1,13 @@
+package netty.packets;
+
+import netty.Packet;
+
+public class StartGamePacket implements Packet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public String toString() {
+		return "start_game";
+	}
+}
