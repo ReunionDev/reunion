8a7963b891888ba3c7170f8bbd548eed017f3a69
Tjeerd Jan van der Molen <aidamina@gmail.com>
2012-11-09 15:55:00 +0100

    * revival packet

---
+++ jreunion/src/test/java/netty/packets/RevivalPacket.java
@@ -0,0 +1,13 @@
+package netty.packets;
+
+import netty.Packet;
+
+public class RevivalPacket implements Packet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public String toString() {
+		return "revival";
+	}
+}
---
+++ jreunion/src/test/java/netty/parsers/RevivalParser.java
@@ -0,0 +1,28 @@
+package netty.parsers;
+
+import java.util.regex.*;
+
+import netty.Packet;
+import netty.packets.RevivalPacket;
+import netty.parsers.PacketParser.Server;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+@Service
+@Scope("prototype")
+@Server
+public class RevivalParser implements PacketParser {
+
+	static final Pattern regex = Pattern.compile("^revival$"); 
+	
+	@Override
+	public Pattern getPattern() {
+		return regex;
+	}
+
+	@Override
+	public Packet parse(Matcher match, String input) {
+		return new RevivalPacket();
+	}
+}
